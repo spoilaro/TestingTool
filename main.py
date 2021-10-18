a5f775c4f80bc619cda5a7550ffbe73b26a33325
Johannes <johannes.haapanen@gmail.com>
2021-10-18 15:56:11 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from typing import List
+from subprocess import run
 
-def read_commands():
+
+def read_commands() -> List[str]:
+    """Reads file of broken commands to a list."""
     parsed_lines = []
 
     with open("./broken-commands.txt", "r") as f:
@@ -11,8 +15,16 @@ def read_commands():
     return parsed_lines
 
 
+def command_runner():
+    """Runs a list of commands given."""
+    output = run("ls", capture_output=True).stdout
+
+
 def main():
+    """Main"""
     broken_commands = read_commands()
+
+    command_runner()
 
 
 if __name__ == "__main__":
